2c4c5837f457236df5f1d2414c7cba3ea0020bc8
michelleACT <michelle@activeon.com>
2016-03-18 19:03:32 +0000

    add s3exception

--- storage/app/delete.php
+++ storage/app/delete.php
@@ -1,17 +1,17 @@
 <?php
 	require '../vendor/autoload.php';
 	include './config.php';
-	
+
 	$key = $_REQUEST['Key'];
-	
+
 	$msg = "success";
 	try {
 		$s3->deleteObject([
 			'Bucket'=>$bucket,
 			'Key'=>$key 
 		]);
-	} catch (Exception $e) {
-	    $msg = 'error : '. $e->getMessage();
+	} catch (S3Exception $e) {
+	    $msg = 'error : '.$e->getMessage();
 	}
 
 	echo $msg;
