ccd95bd249e4a2e2505e3fc95e29cb4507959226
michelleACT <michelle@activeon.com>
2016-03-19 00:55:27 +0000

    add upload and exception

--- storage/app/list.php
+++ storage/app/list.php
@@ -23,9 +23,8 @@
 	  <a href="./logout.php">LogOut</a>
 	</div>
     <div class="buttonDiv">
-      <form action="./upload.php" method="post" enctype="multipart/form-data">
+      <form id="uploadForm"  action="" method="post" enctype="multipart/form-data">
         <input type="file" name="userFile" >
-        <input type="hidden" name="prefix" value="<?=$prefix?>">
         <input type="submit" value="Upload" name="submit">
       </form>
     </div>
@@ -48,7 +47,8 @@
     <ul class="contentsUl">
       <li class="wid180"><?php echo $fileName; ?></li>
       <!-- <li><?php /* echo $result["Metadata"]["a"]; */ ?></li> -->
-      <li class="alignRight"><?php echo date_format($result["LastModified"], 'm-d-Y'); ?></li>
+      <li class="alignRight"><?php echo date_format($result["LastModified"],
+		'm/d/Y'); ?></li>
       <li class="alignRight"><?php echo byteConvert( $result["ContentLength"]); ?></li>
       <li class="alignCenter"><button onclick="deleteByKey('<?php echo
 	  $fileName?>')">Delete</button></li>
@@ -97,6 +97,35 @@
     var url = "download.php?Key=" + key;
     location.href= url;
   }
+
+  $("#uploadForm").on('submit',(function(e){
+	 $.ajax({
+		  type: "POST",
+		  url: "upload.php",
+		  cache: false,
+		  processData:false,
+		  contentType: false,
+		  data: new FormData(this),
+		  success : function(data){
+			  if(data.indexOf("success") > -1){
+				  alert("Upload Success.");
+				  location.reload();
+			  }else{
+				  alert(data);
+			  }
+		  },
+		  error : function(json){
+			  alert("Upload  error : [" + json.status + "] " + json.statusText);
+		  },
+		  beforeSend : function(e){
+			  $("#processLab").text( "Loading..");
+		  },
+		  complete : function(e){
+			  $("#processLab").text("");
+		  }
+	  });
+  }));
+
   </script>
   </body>
 </html>
--- storage/app/upload.php
+++ storage/app/upload.php
@@ -1,30 +1,30 @@
 <?php
+	require '../vendor/autoload.php';
+	include './config.php';
+  	
+	if(isset($_FILES['userFile']['type'])){
+		$prefix = $_SESSION['email'];
 
-  require '../vendor/autoload.php';
+		$key = $prefix."/".$_FILES['userFile']['name'];
+		$expiredDate = date("m/d/Y", strtotime(date("m/d/Y")."+ 1 days ")); 
 
-  $key = $_REQUEST['Key'];
-  $credentials = new Aws\Credentials\Credentials('AKIAIUPNF6WRZ2C7HRIA', 'hxjzxtjtnKYURVSypSrehDh0Qe8FtLmYlQoho/dS');
+		try{
+			$s3->putObject([
+				'ACL'=>'public-read',
+				'SourceFile'=>$_FILES['userFile']['tmp_name'],
+				'Bucket'=>$bucket,
+				'Key'=>$key,
+				'Expires'=>$expiredDate,
+				'Metadata'=>array('resolution'=>'1920*1024','duration'=>'0:0:0')
+			]);
 
-  $s3 = new Aws\S3\S3Client([
-		'version' => '2006-03-01',
-		'region'  => 'us-west-2',
-		'credentials' => $credentials
-        ]);
-
-  $bucket = 'activeon-test-bc';
-  $prefix = $_POST['prefix'];
-  $key = $prefix."/".$_FILES['userFile']['name'];
-  $expiredDate = date("m/d/Y", strtotime(date("m/d/Y")."+ 1 days ")); 
-
-  $s3->putObject([
-	'ACL'=>'public-read',
-	'SourceFile'=>$_FILES['userFile']['tmp_name'],
-	'Bucket'=>$bucket,
-	'Key'=>$key,
-        'Expires'=>$expiredDate,
-	'Metadata'=>array('resolution'=>'1920*1024','duration'=>'0:0:0')
-  ]);
-
-  header('Location: list.php');
+			//header('Location: list.php');
+			echo "success";
+		}catch(S3Exception $e){
+			echo "error : ".$e->getMessage();	
+		}
+	}else{
+		echo "error : file not found.";
+	}
 ?>
 
