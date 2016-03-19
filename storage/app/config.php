33695e6c7ff00de890af73e3d7afc27d18f6f432
michelleACT <michelle@activeon.com>
2016-03-19 01:54:07 +0000

    remove credentials

--- storage/app/config.php
+++ storage/app/config.php
@@ -1,17 +1,14 @@
 <?php
   require '../vendor/autoload.php';
+ 
   use Aws\S3\Exception\S3Exception as S3Exception;
 
   session_start();
 
-  $credentials = new Aws\Credentials\Credentials('AKIAIUOJ5T7PTYKSJQJQ',
-	'XAoYzZ/OzYSO+LW7SAzNFAFhcTWDBrmZTcg0F2iN');
-
   $s3 = new Aws\S3\S3Client([
-		'version' => '2006-03-01',
-		'region'  => 'us-west-2',
-		'credentials' => $credentials
-        ]);
+  			'version' => '2006-03-01',
+			'region'  => 'us-west-2',
+  ]);
 
   $bucket = 'activeon-test-bc';
 
