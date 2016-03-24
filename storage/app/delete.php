45bc3d543c237bcd5db044ecc97c1852b4a0bc14
michelleACT <michelle@activeon.com>
2016-03-24 00:24:40 +0000

    add modified files

--- storage/app/delete.php
+++ storage/app/delete.php
@@ -2,19 +2,33 @@
 	require '../vendor/autoload.php';
 	include './config.php';
 
-	$key = $_REQUEST['Key'];
-	$prefix = $_SESSION['email'];
-
-	$msg = "success";
-	try {
-		$s3->deleteObject([
-			'Bucket'=>$bucket,
-			'Key'=>$prefix."/".$key 
-		]);
-	} catch (S3Exception $e) {
-	    $msg = 'error : '.$e->getMessage();
+	if(!isset($_SESSION['email'])){
+		header("location: ./index.php");
 	}
 
+	$key = $_REQUEST['Key'];
+	$prefix = $_SESSION['email'];
+	
+	$msg = "success";
+	$param = ['Bucket'=>$bucket,'Key'=>$prefix."/".$key];
+	
+	try{	
+		//find file
+		$result = $s3->getObject($param);
+		if($result){
+			//Delete file 
+			try{
+				$s3->deleteObject($param);
+			}catch(S3Exception $e){
+				$msg = "error : [delete] ".$e->getMessage();
+			}
+		}else{
+			$msg = "error : file not found.";
+		}
+	} catch(Exception $e){
+		$msg = $msg."error : [delete:find] ".$e->getMessage();
+	}
+	
 	echo $msg;
 ?>
 
--- storage/app/fn/fn.php
+++ storage/app/fn/fn.php
@@ -3,5 +3,13 @@
     $s = array('Bytes', 'KB', 'MB', 'GB');
     $e = floor(log($bytes)/log(1024)); 
     return sprintf('%.2f '.$s[$e], ($bytes/pow(1024, floor($e)))); 
-  } 
+  }
+
+  
+  function console_log( $data ){
+  	echo '<script>';
+    echo 'console.log('. json_encode( $data ) .')';
+	echo '</script>';
+  }
+
 ?>
